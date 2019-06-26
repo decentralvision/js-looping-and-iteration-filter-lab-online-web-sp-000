2c6d5e9741909464b94b819a079a01271d74127f
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-26 16:53:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  filteredDrivers = drivers.filter(function (driver) { return string === driver; })
+  const filteredDrivers = drivers.filter(function (driver) { return string === driver; })
   console.log()
 }
