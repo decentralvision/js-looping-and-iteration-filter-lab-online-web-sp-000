92e0ae71bb7adcc089691603d241bd8122dfaaab
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-26 17:05:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
   return drivers.filter(function (driver) { return string.toLowerCase() === driver.toLowerCase(); })
-
 }
 
 function fuzzyMatch(drivers, string) {
