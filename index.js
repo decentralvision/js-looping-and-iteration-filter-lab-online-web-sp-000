ccf702a02d696c3cbba479587f46aed098befe36
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-26 16:56:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function findMatching(drivers, string) {
   const filteredDrivers = drivers.filter(function (driver) { return string.toLowerCase() === driver.toLowerCase(); })
   return filteredDrivers
 }
+
+function fuzzyMatch(drivers, string) {
+  
+}
