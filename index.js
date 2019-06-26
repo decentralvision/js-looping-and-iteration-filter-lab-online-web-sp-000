8ceae06c76edf55dfa1c6269339154923ad021f3
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-26 17:34:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function findMatching(drivers, string) {
 }
 
 function fuzzyMatch(drivers, string) {
-  return drivers.filter(function (driver) { return string.toLo})
+  return drivers.filter(function (driver) { return string})
 }
